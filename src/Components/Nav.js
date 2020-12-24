88118f85eedab7d71f185a2e24a1c5e1ff0a8735
Paul Rogers <progers999@hotmail.co.uk>
2020-12-24 12:01:53 +0000

    Styling changes and background added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,6 +8,11 @@ const Nav = () => {
 				<li><a href="#">Home</a></li>
 				<li><a href="#">Shop</a></li>
 				<li><a href="#"><i className="fa fa-shopping-bag"></i></a></li>
+				<li>
+					<a href="https://github.com/beast88/online-store" target="_blank" rel="noreferrer">
+						<i className="fa fa-github"></i>
+					</a>
+				</li>
 			</ul>
 		</nav>
 	)
