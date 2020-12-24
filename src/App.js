065f5f7a0218500a1876d53420dc7d6b407373b8
Paul Rogers <progers999@hotmail.co.uk>
2020-12-24 12:31:28 +0000

    Homepage added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Nav from './Components/Nav'
+import Home from './Components/Home'
 
 const App = () => {
   return(
     <Router>
       <Nav />
+      <Home />
     </Router>
   )
 }
