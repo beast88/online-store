c14ee908ee9a2a7a7a8780b9afd9cf731aeb820a
Paul Rogers <progers999@hotmail.co.uk>
2020-12-28 09:21:46 +0000

    Store navigation built

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Shop = () => {
 	return(
-		<div>
-			<h1>Shop goes here</h1>
-		</div>
+		<main className="shop-container">
+			<aside className="nav-container">
+
+				<ul className="shop-nav">
+					<li>Sofas</li>
+					<li>Tables</li>
+					<li>Bookcases</li>
+					<li>Beds</li>
+				</ul>
+				
+			</aside>
+
+			<div className="shop-front">
+				<h1>Store Items go here</h1>
+			</div>
+		</main>
 	)
 }
 
