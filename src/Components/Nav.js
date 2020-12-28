38cacdf865b11036fcf37e8ec4ba1c9b2445d36a
Paul Rogers <progers999@hotmail.co.uk>
2020-12-28 15:08:48 +0000

    A tag now displays when there are items in the cart

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Nav = (props) => {
+	const displayTotal = props.cartLength > 0 ? 'total-items' : 'total-items hidden'
+
 	return(
 		<nav>
 			<h1>Fakea</h1>
@@ -14,7 +16,10 @@ const Nav = (props) => {
 					<li>Shop</li>
 				</Link>
 
-				<li onClick={props.handleClick}><i className="fa fa-shopping-bag"></i></li>
+				<li className="cart-icon" onClick={props.handleClick}>
+					<i className="fa fa-shopping-bag"></i>
+					<div className={displayTotal}>{props.cartLength}</div>
+				</li>
 
 				<li>
 					<a href="https://github.com/beast88/online-store" target="_blank" rel="noreferrer">
