c549911ebbfeaad00a42b895cd55a1d19cb8d63c
Paul Rogers <progers999@hotmail.co.uk>
2021-02-22 09:51:31 +0000

    Updates

---
+++ precache-manifest.e591a52651f471fac9fb0ed9250076e2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ab17a35a5547ee1a2dd",
+    "url": "/online-store/static/js/main.4ab17a35.chunk.js"
+  },
+  {
+    "revision": "acc1f99eb9be13cc0f57",
+    "url": "/online-store/static/js/1.acc1f99e.chunk.js"
+  },
+  {
+    "revision": "7985bec0124e4ffe2942",
+    "url": "/online-store/static/js/runtime~main.7985bec0.js"
+  },
+  {
+    "revision": "5890554df2514bc3b76c8dc2ac747a8d",
+    "url": "/online-store/static/media/bed1.5890554d.jpg"
+  },
+  {
+    "revision": "b6baa00f43a73f57c13d54111ba1b7ab",
+    "url": "/online-store/static/media/bed2.b6baa00f.jpg"
+  },
+  {
+    "revision": "8492282925000a27f4f528a51bc4e5bb",
+    "url": "/online-store/static/media/bed3.84922829.jpg"
+  },
+  {
+    "revision": "776146496464b71c9feb692d6229ddfa",
+    "url": "/online-store/static/media/bed4.77614649.jpg"
+  },
+  {
+    "revision": "455245511e2631acb31a801f44065091",
+    "url": "/online-store/static/media/bed5.45524551.jpg"
+  },
+  {
+    "revision": "86b3935db5bb42915d600756b989b219",
+    "url": "/online-store/static/media/bed6.86b3935d.jpg"
+  },
+  {
+    "revision": "1c383c410a6bd18073006e19f1264b1a",
+    "url": "/online-store/static/media/bed7.1c383c41.jpg"
+  },
+  {
+    "revision": "65c72f5bbd5ca61edfd629de669d1345",
+    "url": "/online-store/static/media/bookcase1.65c72f5b.jpg"
+  },
+  {
+    "revision": "54df788719eeb3ddb7db21c10f1cf0b2",
+    "url": "/online-store/static/media/bookcase2.54df7887.jpg"
+  },
+  {
+    "revision": "6a2a5d892c470b6ca6a0ed1812568bf1",
+    "url": "/online-store/static/media/bookcase4.6a2a5d89.jpg"
+  },
+  {
+    "revision": "00b947d58b9bfb86c8513539faca319d",
+    "url": "/online-store/static/media/bookcase5.00b947d5.jpg"
+  },
+  {
+    "revision": "570c17b6a1727523f8710f9154a600c8",
+    "url": "/online-store/static/media/sofa1.570c17b6.jpg"
+  },
+  {
+    "revision": "d9a4ec3be672a7209fd746fb0342e39b",
+    "url": "/online-store/static/media/sofa2.d9a4ec3b.jpg"
+  },
+  {
+    "revision": "ac00d28cab821e67197332ada31c81d4",
+    "url": "/online-store/static/media/sofa3.ac00d28c.jpg"
+  },
+  {
+    "revision": "a1badb238e9132109541a21ba3b0ca9a",
+    "url": "/online-store/static/media/sofa4.a1badb23.jpg"
+  },
+  {
+    "revision": "71c634672901a49c440ecfaf9d9deaee",
+    "url": "/online-store/static/media/table1.71c63467.jpg"
+  },
+  {
+    "revision": "5762877398c76415fe40c25ce733dade",
+    "url": "/online-store/static/media/table2.57628773.jpg"
+  },
+  {
+    "revision": "730f29854679e06956e9ccbb122da189",
+    "url": "/online-store/static/media/table3.730f2985.jpg"
+  },
+  {
+    "revision": "714919bff2184fa228450ad98b2ae337",
+    "url": "/online-store/static/media/table4.714919bf.jpg"
+  },
+  {
+    "revision": "cba22c973eabc01d5746d0a5a1247cd6",
+    "url": "/online-store/static/media/table5.cba22c97.jpg"
+  },
+  {
+    "revision": "90578dcbb677248d23e7d9e1c9e38b0e",
+    "url": "/online-store/static/media/table6.90578dcb.jpg"
+  },
+  {
+    "revision": "8c199706ba07bba65044bd8aa3cb403c",
+    "url": "/online-store/index.html"
+  }
+];
